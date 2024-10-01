f8830d9026d7140183626d13f93ce096d837a886
juseok2 <wntjrwnstjr@naver.com>
2024-10-01 11:50:51 +0900

    리스트 출력

---
+++ src/main/java/com/mysite/sbbmybatis/MessageMapper.java
@@ -0,0 +1,12 @@
+package com.mysite.sbbmybatis;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface MessageMapper {
+	Message getMessageById(Integer id);
+	void insertMessage(Message message);
+	List<Message> getMessagesLimit10();
+}
--- src/main/java/com/mysite/sbbmybatis/MessageService.java
+++ src/main/java/com/mysite/sbbmybatis/MessageService.java
@@ -1,12 +1,23 @@
 package com.mysite.sbbmybatis;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MessageService {
+	@Autowired
+	private MessageMapper messageMapper;
 	//DB에서 값을 가져오는것
-	public String getMessage(Integer id) {
-//		return messageMapper.getMessageById(id);
-		return "Hello World~~";
+	public Message getMessage(Integer id) {
+		return messageMapper.getMessageById(id);
+//		return "Hello World~~";
+	}
+	public void createMessage(Message message) {
+        messageMapper.insertMessage(message);
+    }
+	public List<Message> getMessagesLimit10(){
+		return messageMapper.getMessagesLimit10();
 	}
 }
